46545e19de599a0a58e9ec2997d552e12d9169e5
stefanpricopie <stefan.pricopie@postgrad.manchester.ac.uk>
2023-02-02 16:14:51 +0000

    moved to notebooks

--- numpy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# Difference between np.random.random and numpy.random.uniform
-# See https://stackoverflow.com/questions/13213496/what-does-depending-on-rounding-exactly-mean/13213524#13213524
-
-np.random.random()          # has no arguments, returns a float in [0, 1)
-np.random.uniform(5, 10)    # can have arguments, can return float in [5, 10) or in [5, 10] depending on rounding
